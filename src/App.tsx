da2d2962e408a36cdabda30c811b55e0fd5d702e
Wesley Adriann <wesleyadriann@gmail.com>
2020-08-12 14:45:51 -0300

    Adds slick-carousel in dependencies
    Signed-off-by: Wesley Adriann <wesleyadriann@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
 import Routes from './Routes';
 import { GlobalStyle } from './style';
